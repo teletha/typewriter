8f59862a858d3a6cd49fed6648ee901f7c3854fa
teletha <teletha@users.noreply.github.com>
2022-07-06 08:37:22 +0000

    deploy: 1e1324d2f65152f351f5bba2178861a1498ee946

---
+++ root.js
@@ -0,0 +1,165 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"typewriter",
+		"typewriter.api",
+		"typewriter.mongo"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Constraint",
+			"packageName": "typewriter.api",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Constraint.NumericConstraint",
+			"packageName": "typewriter.api",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Constraint.StringConstraint",
+			"packageName": "typewriter.api",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Constraint.TypeConstraint",
+			"packageName": "typewriter.api",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Deletable",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "IdentifiableModel",
+			"packageName": "typewriter",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Mongo",
+			"packageName": "typewriter.mongo",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MongoQuery",
+			"packageName": "typewriter.mongo",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Operatable",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "QueryExecutor",
+			"packageName": "typewriter.api",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Queryable",
+			"packageName": "typewriter.api",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.BooleanSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.CharSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.DateSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.LocalDateSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.LocalDateTimeSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.LocalTimeSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.NumericSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.StringSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Specifier.ZonedDateTimeSpecifier",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Updatable",
+			"packageName": "typewriter.api",
+			"type": "Functional"
+		}
+	]
+}
