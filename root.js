cb9e37f9ddac0a83ed49fd99b9c6ba35abc750a3
teletha <teletha@users.noreply.github.com>
2022-08-01 05:46:39 +0000

    deploy: feab563e98f3a6e82d0f63f93d7d659a7976de99

--- root.js
+++ root.js
@@ -20,13 +20,6 @@ const root = {
 		{
 			"modifiers": {
 				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "ConnectionPool",
-			"packageName": "typewriter.rdb",
-			"type": "Class"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
 			"name": "Constraint",
 			"packageName": "typewriter.api",
 			"type": "Interface"
