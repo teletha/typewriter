0749bdd1b3cd221d52422f5efb5f81be814b544e
Teletha <Teletha.T@gmail.com>
2023-06-24 23:07:36 +0900

    Test more.

---
+++ src/test/java/typewriter/maria/UpdateTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2023 The TYPEWRITER Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package typewriter.maria;
+
+import typewriter.api.UpdateTestSet;
+
+public class UpdateTest extends MariaDBTestBase implements UpdateTestSet {
+}
