70ddfc42f33c562a1211e2c32586eca66cb35ff4
Teletha <Teletha.T@gmail.com>
2024-08-31 12:50:46 +0900

    update readme

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -20,6 +20,7 @@ public class Project extends bee.api.Project {
                 * [H2](https://github.com/h2database/h2database)
                 * [SQLite](https://github.com/sqlite/sqlite)
                 * [MariaDB](https://github.com/MariaDB/)
+                * [PostgreSQL](https://www.postgresql.org/)
                 * [MongoDB](https://github.com/mongodb/mongo)
                 * [DuckDB](https://duckdb.org/)
                 """);
